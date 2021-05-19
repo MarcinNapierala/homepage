9139bb4258fa2ae3b458067fd51a0a762250f180
Marcin Napierala <47379466+MarcinNapierala@users.noreply.github.com>
2021-05-19 16:26:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+        alert('witaj na mojej stronie internetowej')
+        const name = 'Marcin';
+        const age = 39;
+        console.log(name);
+        console.log(age);
+
+
+        console.log(`Nazywam się ${name} 
+        i mam ${age} lat`);
